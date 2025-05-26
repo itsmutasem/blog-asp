7a831a280f9412fe1e12264dcdcd821651823448
Mutasem Mustafa <143445025+itsmutasem@users.noreply.github.com>
2025-05-26 19:13:05 +0300

    Create AuthController.cs

---
+++ blog-asp/BlogApp/Controllers/AuthController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogApp.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Login(string returnUrl = "/")
+        {
+            var redirectUrl = Url.Action("GoogleResponse", "Auth", new { returnUrl });
+            var properties = new AuthenticationProperties { RedirectUri = redirectUrl };
+            return Challenge(properties, "Google");
+        }
+
+        public async Task<IActionResult> GoogleResponse(string returnUrl)
+        {
+            var result = await HttpContext.AuthenticateAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return Redirect(returnUrl);
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
